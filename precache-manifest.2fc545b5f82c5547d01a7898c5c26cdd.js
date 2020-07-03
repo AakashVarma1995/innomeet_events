c3fd8b1e05c63ac172d3eddca701ec2b61842a5c
Aakash Varma <aakash.varma@innowise.us>
2020-07-03 14:28:05 +0530

    new

---
+++ precache-manifest.2fc545b5f82c5547d01a7898c5c26cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33097c6e19b4eb7aa65c2ee0efb468ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f39d0f3ba5d5f1a30710",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b26e24b762fdc55fc577",
+    "url": "/static/js/2.1b895b93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1b895b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f39d0f3ba5d5f1a30710",
+    "url": "/static/js/main.e964ef66.chunk.js"
+  },
+  {
+    "revision": "36b299a417301d621f98",
+    "url": "/static/js/runtime-main.fc1327be.js"
+  }
+]);
